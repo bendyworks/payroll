7eb3babd7af9b0fd6f5a834375a68f4dd541018b
bendycode <stephen@bendyworks.com>
2014-10-22 13:53:53 -0500

    rails g model employee first_name last_name start_date:date end_date:date billable:boolean

---
+++ db/migrate/20141022184946_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :start_date
+      t.date :end_date
+      t.boolean :billable
+
+      t.timestamps
+    end
+  end
+end
