ec0f28dc1752b789f24e1dcb2f873dd40550e4b0
Stephen Anderson and Betsy Lorton <dev+bendycode+schbetsy@bendyworks.com>
2014-11-17 12:09:03 -0600

    allow helper methods to push data into data_table

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -10,19 +10,19 @@ class ChartsController < ApplicationController
     data_table = GoogleVisualr::DataTable.new
     data_table.new_column('date', 'Date')
 
-    create_employee_columns
+    create_employee_columns data_table
     data_table.add_rows(Salary.ordered_dates.count)
-    populate_salary_history_chart_data
+    populate_salary_history_chart_data data_table
     data_table
   end
 
-  def create_employee_columns
+  def create_employee_columns data_table
     Employee.all.each do |employee|
       data_table.new_column('number', employee.first_name)
     end
   end
 
-  def populate_salary_history_chart_data
+  def populate_salary_history_chart_data data_table
     Salary.ordered_dates.each_with_index do |date, date_row_num|
       Employee.all.each do |employee, employee_column_num|
         data_table.set_cell(date_row_num, employee_column_num, employee.salary_on(date))
