485f1b5ac73c572cb215c09cf1a3bb7131e2d222
Betsy Lorton <elorton@gmail.com>
2014-11-18 16:03:52 -0600

    Add Factory Girl

---
+++ spec/factories/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :employee do
+    sequence(:first_name) { |n| "##{n}"}
+    last_name 'Bendyworker'
+    start_date 1.year.ago
+  end
+
+  factory :salary do
+    start_date 1.year.ago
+    annual_amount 700
+    employee
+  end
+end
