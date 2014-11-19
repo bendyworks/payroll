5651dfccc94c2c2fe8dc82daee9196fc71af351c
Betsy Lorton <elorton@gmail.com>
2014-11-19 11:30:39 -0600

    Add (and test) scope for current employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,8 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :start_date, presence: true
 
+  scope :current, -> { where('start_date < ? AND (end_date IS NULL OR end_date > ?)', Date.today, Date.today) }
+
   def employed_on?(date)
     date > start_date && (end_date.nil? || date < end_date)
   end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -87,4 +87,15 @@ RSpec.describe Employee, :type => :model do
       end
     end
   end
+
+  describe 'current' do
+    let!(:normal_employee) { create :employee }
+    let!(:gave_notice) { create :employee, end_date: Date.today + 7 }
+    let!(:past_employee) { create :employee, end_date: Date.today - 7 }
+    let!(:not_started) { create :employee, start_date: Date.today + 14 }
+
+    it 'returns collection of employees employed today' do
+      expect(Employee.current.sort).to eq [gave_notice, normal_employee].sort
+    end
+  end
 end
