02b04677c3b5018f2bc804af79fe6ecf1b07623d
Betsy Lorton <elorton@gmail.com>
2014-11-20 14:25:39 -0600

    Each employee in experience chart is a different color
    - This requires putting each employee's data point in a
      different 'series', each of which has its own tooltip.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -8,7 +8,7 @@ class ChartsController < ApplicationController
   def experience
     opts = { width: 800, height: 500, title: 'Experience vs Salary',
              hAxis: { title: "Years of Experience\n(Time at Bendyworks plus weighted prior experience)", minValue: 0 },
-             vAxis: { title: 'Current Salary', minValue: 0 }, legend: 'none' }
+             vAxis: { title: 'Current Salary', minValue: 0 } }
     @chart = GoogleVisualr::Interactive::ScatterChart.new(experience_chart_data, opts)
   end
 
@@ -64,20 +64,41 @@ class ChartsController < ApplicationController
 
   def experience_chart_data
     data_table = GoogleVisualr::DataTable.new
-    data_table.new_column('number', 'Years at Bendyworks')
-    data_table.new_column('number', 'Current Salary')
-    data_table.new_column('string', 'tooltip text', nil, 'tooltip')
-    data_table.add_rows(Employee.current.count * 2)
+    data_table.new_column('number', 'Years of Experience')
 
-    populate_experience_chart_data! data_table
+    create_employee_columns_with_tooltips!(data_table)
+    populate_experience_chart_data!(data_table)
     data_table
   end
 
+  def create_employee_columns_with_tooltips! data_table
+    Employee.current.each do |employee|
+      data_table.new_column('number', employee.first_name)
+      data_table.new_column('string', 'tooltip text', nil, 'tooltip')
+    end
+  end
+
   def populate_experience_chart_data! data_table
-    Employee.current.each_with_index do |employee, employee_row_num|
-      data_table.set_cell(employee_row_num, 0, employee.weighted_years_experience)
-      data_table.set_cell(employee_row_num, 1, employee.salary_on(Date.today))
-      data_table.set_cell(employee_row_num, 2, "#{employee.first_name}:\n#{employee.all_experience_formatted}\n\$#{employee.salary_on(Date.today)} salary")
+    employees = Employee.current
+
+    employees.each do |employee|
+      data_table.add_rows(1)
+      row = data_table.rows.count - 1
+      data_table.set_cell(row, 0, employee.weighted_years_experience)
+
+      employees.each_with_index do |em, employee_index|
+        if employee == em
+          y_value = employee.salary_on(Date.today)
+          tooltip_text = "#{employee.first_name}:\n#{employee.formatted_experience}\n\$#{employee.salary_on(Date.today)} salary"
+        else
+          y_value = nil
+          tooltip_text = nil
+        end
+
+        employee_column = employee_index * 2 + 1
+        data_table.set_cell(row, employee_column, y_value)
+        data_table.set_cell(row, employee_column + 1, tooltip_text)
+      end
     end
   end
 end
