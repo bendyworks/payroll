4d54f08281459df62f9996f3a860f58eb098341b
Betsy Lorton <elorton@gmail.com>
2014-11-20 10:19:18 -0600

    Include today in history chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -20,9 +20,8 @@ class ChartsController < ApplicationController
     data_table = GoogleVisualr::DataTable.new
     data_table.new_column('date', 'Date')
 
-    create_employee_columns data_table
-    data_table.add_rows(Salary.ordered_dates_with_previous_dates.count)
-    populate_salary_history_chart_data data_table
+    create_employee_columns(data_table)
+    populate_history_chart_data(data_table)
     data_table
   end
 
@@ -32,13 +31,31 @@ class ChartsController < ApplicationController
     end
   end
 
-  def populate_salary_history_chart_data data_table
-    Salary.ordered_dates_with_previous_dates.each_with_index do |date, date_row_num|
+  def populate_history_chart_data data_table
+    populate_salary_changes(data_table)
+    add_salaries_today(data_table)
+  end
+
+  def populate_salary_changes data_table
+    dates = Salary.ordered_dates_with_previous_dates
+    data_table.add_rows(dates.count)
+
+    dates.each_with_index do |date, date_row_num|
       data_table.set_cell(date_row_num, 0, date)
 
       Employee.current.each_with_index do |employee, employee_column_num|
         data_table.set_cell(date_row_num, employee_column_num + 1, employee.salary_on(date))
       end
+    end
+  end
+
+  def add_salaries_today data_table
+    data_table.add_rows(1)
+    row = data_table.rows.count - 1
+    data_table.set_cell(row, 0, Date.today)
+
+    Employee.current.each_with_index do |employee, employee_column_num|
+      data_table.set_cell(row, employee_column_num + 1, employee.salary_on(Date.today))
     end
   end
 
