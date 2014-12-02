27652753263cb619148329d53357d7208a4ec58f
Betsy Lorton <elorton@gmail.com>
2014-12-02 14:50:46 -0600

    Add a new employee
    - Adds routes and controller actions for employee
      new and create
    - Adds link to employee#new in application layout
    - Adds a form view for employee info

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,32 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      starting_salary_params = salary_params.merge(start_date: @employee.start_date)
+      @salary = @employee.salaries.new(starting_salary_params)
+      if @salary.save
+        redirect_to employee_path(@employee), notice: 'Employee successfully created.' and return
+      end
+    end
+    @errors = @employee.errors.any? ? @employee.errors.full_messages : @salary.errors.full_messages
+    render :new
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :start_date,
+                                     :direct_experience, :indirect_experience, :billable)
+  end
+
+  def salary_params
+    params.require(:employee).permit(:annual_amount)
+  end
 end
