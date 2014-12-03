4b4871dd7a4085897ecfc6f530e8579a566eb3a1
Betsy Lorton <elorton@gmail.com>
2014-12-03 16:06:38 -0600

    Starting salary info now updated automatically
    - starting salary is now a field on employee, so
      we don't need special tests to ensure it gets updated

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -16,22 +16,4 @@ RSpec.describe EmployeesController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe 'PATCH update' do
-    let!(:salary) { create :salary }
-    let!(:employee) { salary.employee }
-
-    context 'updating employee start date' do
-      let(:employee_params) {  employee.attributes.merge('start_date' => Date.today) }
-      let(:update_params) { { id: employee.id, employee: employee_params } }
-
-      it 'updates starting salary start_date to match' do
-        patch :update, update_params
-        salary.reload
-
-        expect(salary.start_date).to eq Date.today
-      end
-    end
-  end
-
 end
