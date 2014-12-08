059e47e63866e9ffe458eac534c42995eee34148
Betsy Lorton <elorton@gmail.com>
2014-12-08 14:10:47 -0600

    Private method got misplaced in rebase

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,29 +8,6 @@ class EmployeesController < ApplicationController
     @chart = GoogleVisualr::Interactive::LineChart.new(employee_salary_history_data, opts)
   end
 
-  private
-
-  def employee_salary_history_data
-    data_table = GoogleVisualr::DataTable.new
-    data_table.new_column('date', 'Date')
-    data_table.new_column('number', @employee.first_name)
-
-    data_table.add_rows( [ [@employee.start_date, @employee.starting_salary] ] )
-
-    @employee.salaries.each_with_index do |salary, index|
-      data_table.add_rows( [ [salary.start_date - 1, @employee.salary_on(salary.start_date - 1)],
-                             [salary.start_date, salary.annual_amount] ] )
-    end
-
-    if @employee.end_date
-      data_table.add_rows( [ [@employee.end_date, @employee.ending_salary] ] )
-    elsif @employee.employed_on?(Date.today)
-      data_table.add_rows( [ [Date.today, @employee.salary_on(Date.today)] ] )
-    end
-
-    data_table
-  end
-
   def new
     @employee = Employee.new
   end
@@ -64,5 +41,28 @@ class EmployeesController < ApplicationController
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :start_date, :starting_salary,
                                      :direct_experience, :indirect_experience, :billable)
+  end
+
+  private
+
+  def employee_salary_history_data
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('date', 'Date')
+    data_table.new_column('number', @employee.first_name)
+
+    data_table.add_rows( [ [@employee.start_date, @employee.starting_salary] ] )
+
+    @employee.salaries.each_with_index do |salary, index|
+      data_table.add_rows( [ [salary.start_date - 1, @employee.salary_on(salary.start_date - 1)],
+                             [salary.start_date, salary.annual_amount] ] )
+    end
+
+    if @employee.end_date
+      data_table.add_rows( [ [@employee.end_date, @employee.ending_salary] ] )
+    elsif @employee.employed_on?(Date.today)
+      data_table.add_rows( [ [Date.today, @employee.salary_on(Date.today)] ] )
+    end
+
+    data_table
   end
 end
