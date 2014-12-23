762e3fc57b4edd1c79450dbba764913e29504813
Betsy Lorton <elorton@gmail.com>
2014-12-23 16:07:18 -0600

    Strong params for chart filter form

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,9 +1,18 @@
 class ChartsController < ApplicationController
   def history
-    @chart = HistoryChart.new(params).chart
+    @chart = HistoryChart.new(chart_params).chart
   end
 
   def experience
-    @chart = ExperienceChart.new(params).chart
+    @chart = ExperienceChart.new(chart_params).chart
+  end
+
+  private
+
+  def chart_params
+    employment = params[:employment].try(:permit, :past, :current, :future)
+    billable = params[:billable].try(:permit, :true, :false)
+    owner = params[:owner].try(:permit, :true, :false)
+    {employment: employment, billable: billable, owner: owner}
   end
 end
