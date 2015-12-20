2e727facb215b0d90e656833a7d2a8ebc6ab381e
bendycode <stephen@bendyworks.com>
2015-12-20 15:46:43 -0600

    rubocop -a --only Style/EmptyLineBetweenDefs

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -11,18 +11,23 @@ class Employee < ActiveRecord::Base
   def self.current
     where 'start_date <= ? AND (end_date IS NULL OR end_date >= ?)', Date.today, Date.today
   end
+
   def self.non_current
     where 'start_date > ? OR end_date < ?', Date.today, Date.today
   end
+
   def self.future
     where 'start_date > ?', Date.today
   end
+
   def self.past
     where 'end_date < ?', Date.today
   end
+
   def self.billed
     where billable: true
   end
+
   def self.support
     where billable: false
   end
