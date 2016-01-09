964159a267192958ff0ab8906aa327a066d3636c
bendycode <stephen@bendyworks.com>
2016-01-09 16:37:14 -0600

    rubocop fixes

--- app/models/balance_csv_parser.rb
+++ app/models/balance_csv_parser.rb
@@ -6,29 +6,46 @@ class BalanceCsvParser
     record_accounts account_hashes
   end
 
+  private
+
   def self.record_accounts(account_hashes)
     account_hashes.each do |account_hash|
       record_account account_hash
     end
   end
+  private_class_method :record_accounts
 
   def self.record_account(account_hash)
     name = account_hash.delete(:account) || fail('Missing account name')
     account = Account.find_by_name(name) || fail("No account for '#{name}'")
 
     account_hash.each do |date_sym, balance_string|
-      begin
-        date = Date.strptime(date_sym.to_s, '%m/%d/%Y')
-      rescue ArgumentError
-        raise ArgumentError, "Invalid Date: #{date_sym}"
-      end
-      if balance_string.is_a?(String)
-        amount = balance_string.gsub(/[,$]/, '').to_d || fail('Missing amount')
-      else
-        amount = balance_string
-      end
-      balance = Balance.find_or_initialize_by(account_id: account.id, date: date)
-      balance.update_attributes! amount: amount
+      record_account_entry account, date_sym, balance_string
     end
   end
+  private_class_method :record_account
+
+  def self.record_account_entry(account, date_sym, balance_string)
+    date = parse_date(date_sym)
+    amount = parse_amount(balance_string)
+    balance = Balance.find_or_initialize_by(account_id: account.id, date: date)
+    balance.update_attributes! amount: amount
+  end
+  private_class_method :record_account_entry
+
+  def self.parse_date(date_sym)
+    Date.strptime(date_sym.to_s, '%m/%d/%Y')
+  rescue ArgumentError
+    raise ArgumentError, "Invalid Date: #{date_sym}"
+  end
+  private_class_method :parse_date
+
+  def self.parse_amount(balance_string)
+    if balance_string.is_a?(String)
+      balance_string.gsub(/[,$]/, '').to_d || fail('Missing amount')
+    else
+      balance_string
+    end
+  end
+  private_class_method :parse_amount
 end
