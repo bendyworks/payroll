53ae2d2a17868d48c71fae194ea137dd1c97ea8f
bendycode <stephen@bendyworks.com>
2016-02-06 23:25:36 -0600

    Remove unused json API

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -23,35 +23,24 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(account_params)
 
-    respond_to do |format|
-      if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render :show, status: :created, location: @account }
-      else
-        format.html { render :new }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.save
+      redirect_to @account, notice: 'Account was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @account.update(account_params)
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account }
-      else
-        format.html { render :edit }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.update(account_params)
+      redirect_to @account, notice: 'Account was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @account.destroy
-    respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Account was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_path, notice: 'Account was successfully destroyed.'
   end
 
   private
