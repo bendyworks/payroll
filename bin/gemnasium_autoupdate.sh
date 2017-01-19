94ebe303feaa3b52af0ef3a079aa213bc71eceae
bendycode <stephen@bendyworks.com>
2017-01-19 15:27:25 -0600

    Gemnasium instructions changed

--- bin/gemnasium_autoupdate.sh
+++ bin/gemnasium_autoupdate.sh
@@ -3,6 +3,6 @@
 # newest commit has to already be pushed, and analyzed by gemnasium first
 
 export GEMNASIUM_TESTSUITE="bundle exec rake"
-export GEMNASIUM_PROJECT_SLUG="bendyworks/payroll"
+export GEMNASIUM_PROJECT_SLUG="github.com/bendyworks/payroll"
 gemnasium --token 250779aa3cc41b7cf33714dee328f2b6 autoupdate run
 open https://github.com/bendyworks/payroll/pulls
