642c9861033e4ab983b91e839a9c5cb656c39489
bendycode <stephen@bendyworks.com>
2017-01-26 16:50:33 -0600

    test edit planning fields in place

--- features/step_definitions/planning_steps.rb
+++ features/step_definitions/planning_steps.rb
@@ -19,11 +19,35 @@ Then(/^I see planning information for those employees$/) do
           expected_content = employee.send(field)
           expected_content = expected_content.strftime('%m/%d/%y') if expected_content.class == Date
 
-          within "##{field}" do
-            expect(page).to have_content(expected_content)
-          end
+          verify_field_value field, expected_content
         end
       end
     end
+  end
+end
+
+When(/^I edit planning fields in place$/) do
+  in_place_edit 'planning_raise_date', '01/14/2018'
+  in_place_edit 'planning_raise_salary', '15976'
+  in_place_edit 'planning_notes', 'What do you think?'
+end
+
+Then(/^I see my planning field changes$/) do
+  verify_field_value 'planning_raise_date', '01/14/2018'
+  verify_field_value 'planning_raise_salary', '15976'
+  verify_field_value 'planning_notes', 'What do you think?'
+end
+
+private
+
+def in_place_edit(field, value)
+  find("##{field} > span").click
+  fill_in field, with: value
+  page.find('body').click # trigger blur
+end
+
+def verify_field_value(field, expected_content)
+  within "##{field}" do
+    expect(page).to have_content(expected_content)
   end
 end
