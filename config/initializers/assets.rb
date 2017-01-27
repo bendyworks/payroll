e6bce287f853b76efeb3f4049b53cb9545fcc71b
bendycode <stephen@bendyworks.com>
2017-01-27 16:56:44 -0600

    Move vendor JS to vendor directory
    - Keep code climate from reporting on JS that's not our code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(
+  sorttable.js
   home.js
-  shared/sorttable.js
   experience.js
   planning.js
   salaries.js
