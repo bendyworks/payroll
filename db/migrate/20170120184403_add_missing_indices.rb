e9a286c9d50ec365ac18a07c8091026db7c49c11
Amy Terry-Penak <amy@bendyworks.com>
2019-12-18 10:52:21 -0600

    Check if index exists

--- db/migrate/20170120184403_add_missing_indices.rb
+++ db/migrate/20170120184403_add_missing_indices.rb
@@ -2,7 +2,7 @@
 
 class AddMissingIndices < ActiveRecord::Migration[6.0]
   def change
-    add_index :accounts, :account_type_id
+    add_index :accounts, :account_type_id unless index_exists?(:accounts, :account_type_id)
     add_index :salaries, :employee_id
     add_index :users, [:invited_by_id, :invited_by_type]
   end
