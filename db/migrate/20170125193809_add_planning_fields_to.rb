c9a853df0670c458c56b2c2cc7405cdf80202f5d
Amy Terry-Penak <amy@bendyworks.com>
2019-12-18 16:10:01 -0600

    Change proposed raise field to a string

--- db/migrate/20170125193809_add_planning_fields_to.rb
+++ db/migrate/20170125193809_add_planning_fields_to.rb
@@ -3,7 +3,7 @@
 class AddPlanningFieldsTo < ActiveRecord::Migration[6.0]
   def change
     add_column :employees, :planning_raise_date, :date
-    add_column :employees, :planning_raise_salary, :decimal
+    add_column :employees, :planning_raise_salary, :string
     add_column :employees, :planning_notes, :text
   end
 end
