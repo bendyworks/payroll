c5dda8e65b97e58676e06cf9ea37694e823ee62c
Amy Terry-Penak <amy@bendyworks.com>
2019-12-18 11:41:06 -0600

    Check that indexes exist before adding them

--- db/migrate/20170120184403_add_missing_indices.rb
+++ db/migrate/20170120184403_add_missing_indices.rb
@@ -3,7 +3,8 @@
 class AddMissingIndices < ActiveRecord::Migration[6.0]
   def change
     add_index :accounts, :account_type_id unless index_exists?(:accounts, :account_type_id)
-    add_index :salaries, :employee_id
-    add_index :users, [:invited_by_id, :invited_by_type]
+    add_index :salaries, :employee_id unless index_exists?(:salaries, :employee_id)
+    invited_by_params = [:invited_by_id, :invited_by_type]
+    add_index :users, invited_by_params unless index_exists?(:users, invited_by_params)
   end
 end
