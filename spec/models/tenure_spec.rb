d2feda7f3509a84cc0a1e113fb1aeefac0d848f6
Amy Terry-Penak <amy@bendyworks.com>
2021-03-04 12:05:53 -0600

    Add more tenure model tests

--- spec/models/tenure_spec.rb
+++ spec/models/tenure_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Tenure do  
+describe Tenure do
   it { should belong_to :employee }
   it { should validate_presence_of :start_date }
   it { should validate_presence_of :employee }
@@ -44,6 +44,28 @@ describe Tenure do
       tenure = employee.tenures.first
       tenure.end_date = Time.zone.today - 3
       expect(tenure).to be_invalid
+    end
+  end
+
+  context 'ordered dates' do
+    let(:first_start_date) { Time.zone.today - 14 }
+    let(:second_start_date) { Time.zone.today - 7 }
+    let(:first_end_date) { Time.zone.today - 3 }
+    let(:second_end_date) { Time.zone.today + 7 }
+
+    let!(:first_tenure) { create :tenure, start_date: first_start_date, end_date: first_end_date }
+    let!(:second_tenure) { create :tenure, start_date: first_start_date, end_date: second_end_date }
+    let!(:third_tenure) { create :tenure, start_date: second_start_date }
+
+    it 'returns all start dates in order' do
+      expect(Tenure.ordered_start_dates).to eq([first_start_date, second_start_date])
+    end
+
+    it 'returns all end dates in order with next dates' do
+      expect(Tenure.ordered_end_dates_with_next_dates).to eq([first_end_date,
+                                                              first_end_date + 1,
+                                                              second_end_date,
+                                                              second_end_date + 1])
     end
   end
 end
