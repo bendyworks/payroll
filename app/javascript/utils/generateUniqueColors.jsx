d98b76e090affd3da1159c5133bad57c6fa7b928
Tanner Hartwig <tanner.hartwig57@gmail.com>
2021-03-25 18:41:22 -0500

    generateUniqueColors

---
+++ app/javascript/utils/generateUniqueColors.jsx
@@ -0,0 +1,11 @@
+export function generateUniqueColors(amount, saturation = 100, lightness = 50, alpha = 1) {
+  let colors = [];
+  let huedelta = Math.trunc(360 / amount);
+
+  for (let i = 0; i < amount; i++) {
+    let hue = i * huedelta;
+    colors.push(`hsla(${hue},${saturation}%,${lightness}%,${alpha})`);
+  }
+
+  return colors;
+}
