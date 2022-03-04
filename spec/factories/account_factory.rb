c9dd877d930233da311f1b75b1ea043056a5acd0
Ben Dockery <ben.dockery@bendyworks.com>
2022-03-04 15:46:28 -0500

    Removes old cucumber tests and resets rspec folder

--- spec/factories/account_factory.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :account do
-    sequence(:name) { |n| "Account ##{n}" }
-    account_type { create :account_type }
-  end
-end
--- spec/models/account_type_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe AccountType, type: :model do
-  it { should validate_presence_of :name }
-  it { should validate_uniqueness_of :name }
-end
