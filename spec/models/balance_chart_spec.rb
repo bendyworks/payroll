0febb33df75623464eeb044d991186369cd00e9e
Ben Dockery <ben.dockery@bendyworks.com>
2022-03-07 09:34:57 -0500

    Adds files for future tests. Changes ci for rspec.

---
+++ spec/models/balance_chart_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe BalanceChart, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/models/salary_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Salary, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
